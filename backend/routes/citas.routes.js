525d7a3fb467e9caf98f93738ad476c2f4f17a8b
ReloJunior <andresrodriguezab@hotmail.com>
2024-04-29 19:30:48 -0300

    Agregando privacidad a los endpoints de medico

--- backend/routes/citas.routes.js
+++ backend/routes/citas.routes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const citaRouter = express.Router()
 const citaController = require('../controllers/cita.controller')
-const verifyToken = require('../middleware/authMiddleware')
 
 citaRouter.post('/', citaController.crearCita)
 citaRouter.get('/', citaController.obtenerCitas)
--- backend/routes/medico.routes.js
+++ backend/routes/medico.routes.js
@@ -3,8 +3,8 @@ const medicoRouter = express.Router()
 const medicoController = require('../controllers/medico.controller')
 const verifyToken = require('../middleware/authMiddleware')
 
-medicoRouter.post('/', medicoController.agregarMedico)
-medicoRouter.get('/', medicoController.obtenerMedicos)
-medicoRouter.get('/:medicoId', medicoController.obtenerMedicoPorId)
-medicoRouter.delete('/:medicoId', medicoController.eliminarMedico)
+medicoRouter.post('/',verifyToken, medicoController.agregarMedico)
+medicoRouter.get('/',verifyToken, medicoController.obtenerMedicos)
+medicoRouter.get('/:medicoId',verifyToken, medicoController.obtenerMedicoPorId)
+medicoRouter.delete('/:medicoId',verifyToken, medicoController.eliminarMedico)
 module.exports = medicoRouter
